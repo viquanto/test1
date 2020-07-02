95735af65a6d882e4cff790ef08562062158e003
viquanto <67706771+viquanto@users.noreply.github.com>
2020-07-02 17:19:26 +0200

    Update startfile.R
    änderungen mittendrin

--- startfile.R
+++ startfile.R
@@ -2,3 +2,15 @@
 ## Ziehung der Lottozahlen für lotto6aus49 und eurojackpot (öffentliches PJ)
 ##
  
+## So, hier soll jetzt nichts mehr rein.
+
+## 5 aus 50 plus 2 Eurozahlen
+eurojackpot <- function(){
+    
+    hz <- sort(sample(1:51, 5))                ## dflt: replace=FALSE in sample(1:49, 6, replace=FALSE), d.h. ohne Zurücklegen
+    ez <- sample(1:10, 2)   
+    
+    result0 <- paste0("5 aus 50 Tipp: ",Reduce("paste",hz)," ... EZ ",Reduce("paste",ez)) 
+    
+    return(result)
+}
